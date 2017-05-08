210bd0e4b15b8ff72e5ff40de042248997931883
jfrade <jfrade@Abra.local>
2017-05-08 10:15:15 -0400

    lazyload images for project pages

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-function init() {
-    var imgDefer = document.getElementsByTagName('img');
-    for (var i = 0; i < imgDefer.length; i++) {
-        if (imgDefer[i].getAttribute('data-src')) {
-            imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
-        }
-    }
-}
-window.onload = init;
+// function init() {
+//     var imgDefer = document.getElementsByTagName('img');
+//     for (var i = 0; i < imgDefer.length; i++) {
+//         if (imgDefer[i].getAttribute('data-src')) {
+//             imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
+//         }
+//     }
+// }
+// window.onload = init;
